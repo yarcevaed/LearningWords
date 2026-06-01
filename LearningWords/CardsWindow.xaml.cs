44b8f3dd1eef40ba8511c2b25e6e9428f510efd2
yarcevaed <yarcevaed@yandex.ru>
2026-06-01 15:10:12 +0300

    Добавьте файлы проекта.

---
+++ LearningWords/CardsWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System.Windows;
+using System.Text.Json;
+using System.IO;
+namespace LearningWords
+{
+    /// <summary>
+    /// Логика взаимодействия для CardsWindow.xaml
+    /// </summary>
+    public partial class CardsWindow : Window
+    {
+        //жалкая пародия на сохранение
+        private AppSetings _settings;
+        private string _settingsFile = "settings.json";
+        public CardsWindow() //здесь каша-мала: и сейв, и прочая хуета
+        {
+            InitializeComponent();
+            LoadSettings();
+            ApplySettingsToUI();
+        }
+        public class AppSetings //то, что будет по дефолту
+        {
+            public int _learned_words { get; set; } = 0;
+
+            public List<string> _words { get; set; }  = new List<string>
+            {
+                "Кошка", "Собака", "Яблоко", "Холодильник", "Гриб", "Арбуз", "Стул", "Клубника",
+                "Здание", "Клавиатура", "Знание", "Рыба", "Волосы", "Ключ", "Преступление", "Адвокат",
+                "Сосед", "Друг", "Будущее", "Голова", "Цвет"
+            }; 
+
+            public List<string> _translation { get; set; } = new List<string>
+            {
+                "Cat", "Dog", "Apple", "Fridge", "Mushroom", "Watermelon", "Chair", "Strawberry",
+                "Building", "Keyboard", "Knowledge", "Fish", "Hair", "Key", "Crime", "Lawyer",
+                "Neighbour", "Friend", "Future", "Head", "Color"
+            };
+            public List<string> _save_words { get; set; } = new List<string>
+            {
+
+            };
+            public List<string> _save_translation { get; set; } = new List<string>
+            {
+
+            };
+
+        }
+        private void LoadSettings() //непонятная хуета, но нужная
+        {
+            if (File.Exists(_settingsFile))
+            {
+                string json = File.ReadAllText(_settingsFile);
+                _settings = JsonSerializer.Deserialize<AppSetings>(json);
+            }
+            if (_settings == null)
+            {
+                _settings = new AppSetings();
+                
+            }
+        }
+        public void ApplySettingsToUI() //чтение
+        {
+            learned_words = _settings._learned_words;
+            words = _settings._words;
+            translation = _settings._translation;
+            save_words = _settings._save_words;
+            save_translation = _settings._save_translation;
+
+        }
+        private void SaveSettings() //сохранение данных !!!ОБЯЗАТЕЛЬНО ВЫЗВАТЬ!!!
+        {
+            _settings._learned_words = learned_words;
+            _settings._words = words;
+            _settings._translation = translation;
+            _settings._save_words = save_words;
+            _settings._save_translation = save_translation;
+            string json = JsonSerializer.Serialize(_settings);
+            File.WriteAllText("settings.json", json);
+        }
+
+
+        public void Her(object sender, RoutedEventArgs e) //Loaded, запускается автоматически вместе с окном
+        {
+            LearnedWords.Text = "⭐ " + learned_words.ToString() + " слов выучено";  
+        }
+
+        //---------SHOW CARDS------------
+
+        int click = 0;
+        List<string> words = new List<string>
+        {
+                "Кошка", "Собака", "Яблоко", "Холодильник", "Гриб", "Арбуз", "Стул", "Клубника",
+                "Здание", "Клавиатура", "Знание", "Рыба", "Волосы", "Ключ", "Преступление", "Адвокат",
+                "Сосед", "Друг", "Будущее", "Голова", "Цвет"
+        };
+        List<string> translation = new List<string>
+        {
+                "Cat", "Dog", "Apple", "Fridge", "Mushroom", "Watermelon", "Chair", "Strawberry",
+                "Building", "Keyboard", "Knowledge", "Fish", "Hair", "Key", "Crime", "Lawyer",
+                "Neighbour", "Friend", "Future", "Head", "Color"
+        };
+        public static List<string> save_words = new List<string>
+        {
+             
+        };
+        public static List<string> save_translation = new List<string>
+        {
+            
+        };
+        Random random = new Random();
+        int count;
+        public static int learned_words = 0;
+        public void ShowCards(object sender, RoutedEventArgs e)
+        {
+            if (words.Count == 0)
+            {
+                Card.Style = (Style)FindResource("BlockButtonStyle");
+                Card.Content = "ПОЗДРАВЛЯЕМ!\nВы прошли все карточки!";
+                click = -1;
+            }
+            if (click == 0)
+            {
+                count = random.Next(words.Count);
+            }
+            if (Card.Content == "Начать")
+            {
+                Card.Content = words[count];
+                ++click;
+            }
+            else if (click == 0)
+            {
+                Card.Content = words[count];
+                Card.Style = (Style)FindResource("BlockButtonStyle");
+                ++click;
+            }
+            else if (click == 1)
+            {
+                Card.Content = translation[count];
+                Card.Style = (Style)FindResource("BlockButtonStyle2");
+                save_words.Add(words[count]);
+                save_translation.Add(translation[count]);
+                words.RemoveAt(count);
+                translation.RemoveAt(count);
+                click = 0;
+                ++learned_words;
+                LearnedWords.Text = "⭐ " + learned_words.ToString() + " слов выучено"; 
+                SaveSettings();
+            }
+        }
+        //------MENUE BUTTONS------
+
+        public void MenueButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenueWindow _menue = new MenueWindow();
+            _menue.OwnerCardsWindow = this;
+            _menue.Show();
+        }
+
+        public void CardsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void DictionaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            DictionaryWindow _dictionary = new DictionaryWindow();
+            _dictionary.OwnerCardsWindow = this;
+            _dictionary.Show();
+        }
+
+        public void StatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
+
---
+++ LearningWords/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System.IO;
+using System.Runtime;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static LearningWords.CardsWindow;
+
+namespace LearningWords
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MenueWindow : Window
+    {
+        // СОХРАНЕНИЕ (дура не удаляй, оно должно читать файл)
+
+        private AppSetings _settings;
+        private string _settingsFile = "settings.json";
+        public MenueWindow()
+        {
+            InitializeComponent();
+            LoadSettings();
+            ApplySettingsToUI();
+
+        }
+        public class AppSetings //то, что будет по дефолту
+        {
+            public int _learned_words { get; set; } = 0;
+
+            public List<string> _words { get; set; } = new List<string>
+            {
+                "Кошка", "Собака", "Яблоко", "Холодильник", "Гриб", "Арбуз", "Стул", "Клубника",
+                "Здание", "Клавиатура", "Знание", "Рыба", "Волосы", "Ключ", "Преступление", "Адвокат",
+                "Сосед", "Друг", "Будущее", "Голова", "Цвет"
+            };
+
+            public List<string> _translation { get; set; } = new List<string>
+            {
+                "Cat", "Dog", "Apple", "Fridge", "Mushroom", "Watermelon", "Chair", "Strawberry",
+                "Building", "Keyboard", "Knowledge", "Fish", "Hair", "Key", "Crime", "Lawyer",
+                "Neighbour", "Friend", "Future", "Head", "Color"
+            };
+            public List<string> _save_words { get; set; } = new List<string>
+            {
+
+            };
+            public List<string> _save_translation { get; set; } = new List<string>
+            {
+
+            };
+
+        }
+        private void LoadSettings() //непонятная хуета, но нужная
+        {
+            if (File.Exists(_settingsFile))
+            {
+                string json = File.ReadAllText(_settingsFile);
+                _settings = JsonSerializer.Deserialize<AppSetings>(json);
+            }
+            if (_settings == null)
+            {
+                _settings = new AppSetings();
+
+            }
+        }
+        public void ApplySettingsToUI() //чтение
+        {
+            learned_words = _settings._learned_words;
+            save_words = _settings._save_words;
+            save_translation = _settings._save_translation;
+
+        }
+        //------MENUE BUTTONS------
+
+        public CardsWindow OwnerCardsWindow {  get; set; }
+        public int learned_words = CardsWindow.learned_words;
+
+        public void Her(object sender, RoutedEventArgs e)
+        {
+            LearnedWords.Text = "⭐ " + learned_words.ToString() + " слов выучено";
+        }
+        public void MenueButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public void CardsButton_Click(object sender, RoutedEventArgs e)
+        {
+            CardsWindow _card = new CardsWindow();
+            _card.Show();
+        }
+
+        public void DictionaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            DictionaryWindow _dictionary = new DictionaryWindow();
+            _dictionary.Show();
+        }
+
+        public void StatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //------DEFOULT BUTTONS------
+
+        public void Cards_Click(object sender, RoutedEventArgs e)
+        {
+            CardsButton_Click(sender, e);
+        }
+
+        public void Dictionary_Click(object sender, RoutedEventArgs e)
+        {
+            DictionaryButton_Click(sender, e);
+        }
+
+        public void Repetition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Test_Click(object sender, RoutedEventArgs e)
+        {
+            TestButton_Click(sender, e);
+        }
+    }
+}
